4187cc62c6f69bf87bdab28b056016757fc364fe
Belmaxi <88465067+Belmaxi@users.noreply.github.com>
2024-05-19 14:48:01 +0800

    更新攻击区域

---
+++ Assets/Scripts/Mechanics/AttackController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Platformer.Mechanics
+{
+    public class AttackController : MonoBehaviour
+    {
+        public float damage;
+        public Collider2D[] attackAreas;
+
+        public Dictionary<string, Collider2D> attackAreasDict = new Dictionary<string, Collider2D>();
+        private void Awake()
+        {
+            attackAreas = GetComponentsInChildren<Collider2D>();
+        }
+
+        private void Start()
+        {
+            foreach(Collider2D area in attackAreas)
+            {
+                attackAreasDict.Add(area.name, area);
+            }
+            
+        }
+        public void Update()
+        {
+
+        }
+
+        public virtual void Handle()
+        {
+
+        }
+    }
+}
+
