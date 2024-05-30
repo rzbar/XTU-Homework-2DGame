1282ce6648016f4e8b8e72d81e677d1736af1e9e
“rzbar” <202205501711@smail.xtu.edu.cn>
2024-05-30 22:40:31 +0800

    Boss技能改enit

--- Assets/Scripts/BossSkill/FallDown.cs
+++ Assets/Scripts/BossSkill/FallDown.cs
@@ -1,26 +1,58 @@
+using Platformer.Gameplay;
 using Platformer.Mechanics.Skill;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 namespace Platformer.Mechanics.Skill
 {
     public class FallDown : SkillEmitter
     {
-        public EnemyController enemy;
+        private Boss0 enemy;
+        private PlayerController player;
+
+        public GameObject waringArea;
+        private float nowHeight;
+        public float jumpHeight;
+        public float jumpSpeed;
+
+        private float leftBound;
+        private float rightBound;
         public override void Emit()
         {
-            var obj = Instantiate(model.attackScriptableObject.attackObjects[0],enemy.transform.position,Quaternion.identity,null);
-            
-            
+            enemy = GameObject.Find("Boss0").gameObject.GetComponent<Boss0>();
+            player=model.player;
+            leftBound = enemy.path.transform.position.x + enemy.path.startPosition.x + 1.2f;
+            rightBound = enemy.path.transform.position.x + enemy.path.endPosition.x - 1.2f;
+            nowHeight = enemy.transform.position.y;
+            jumpHeight += enemy.transform.position.y;
+            StartCoroutine(FallDownSkill());
         }
 
-        IEnumerator Invincibility()
+        IEnumerator FallDownSkill()
         {
-            model.player.Invincibility++;
-            yield return new WaitForSeconds(60 * Time.deltaTime);
-            model.player.Invincibility--;
+            while (enemy.transform.position.y < jumpHeight)
+            {
+                enemy.control.velocity.y = jumpSpeed;
+                yield return new WaitForEndOfFrame();
+            }
+            print(8);
+            enemy.transform.position = new Vector2(Mathf.Clamp(player.transform.position.x, leftBound, rightBound), jumpHeight);
+            var attack = Instantiate(waringArea);
+            attack.transform.position = new Vector2(enemy.transform.position.x, nowHeight - 1);
+            var attackArea = attack.transform.GetChild(0);
+            while (enemy.transform.position.y > nowHeight)
+            {
+                enemy.control.velocity.y = -jumpSpeed;
+                float dis = enemy.transform.position.y - 0.1f;
+                attackArea.transform.localScale = new Vector3(1 - dis / 20, 1 - dis / 20, 1);
+                yield return new WaitForEndOfFrame();
+            }
+            Destroy(attack);
         }
+
+        
     }
 }
 
--- Assets/Scripts/Enemy/Boss0.cs
+++ Assets/Scripts/Enemy/Boss0.cs
@@ -2,40 +2,33 @@ using Platformer.Mechanics;
 using Platformer.Mechanics.Skill;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 [RequireComponent(typeof(SkillManager))]
 public class Boss0 : FollowEnemy
 {
     public float waitTime;
     private float fromPlayer;
-    public float jumpHeight;
-    public float jumpSpeed;
-    private float nowHeight;
-    public SkillManager skillManager;
-    public GameObject waringArea;
+
     public GameObject rushAtea;
     public float rushDis;
+
+    public SkillManager skillManager;
     float timer = 0;
     public GameObject bossUI;
-    private float leftBound;
-    private float rightBound;
     public GameObject rushEffect;
     protected override void Awake()
     {
         base.Awake();
         skillManager = GetComponent<SkillManager>();
         rushEffect.SetActive(false);
-        jumpHeight += transform.position.y;
-        nowHeight = transform.position.y;
         isStop = true;
-        leftBound =path.transform.position.x + path.startPosition.x + 1.2f;
-        rightBound = path.transform.position.x + path.endPosition.x - 1.2f;
     }
     protected override void Update()
     {
         if (player.transform.position.x > path.transform.position.x + path.startPosition.x && player.transform.position.x < path.transform.position.x + path.endPosition.x)
         {
-            if (player.transform.position.y < path.transform.position.y + path.startPosition.y + jumpHeight + 1 && player.transform.position.y > path.transform.position.y + path.startPosition.y - 1)
+            if (player.transform.position.y < path.transform.position.y + path.startPosition.y + 10+ 1 && player.transform.position.y > path.transform.position.y + path.startPosition.y - 1)
             {
                 isStop = false;
             }
@@ -53,12 +46,12 @@ public class Boss0 : FollowEnemy
             {
                 if (fromPlayer > distance)
                 {
+                    RandomSkill(new int[1] { 3 });
                     timer = 0;
-                    StartCoroutine(FallDown());
                 }
                 else if (fromPlayer < 4)
                 {
-                    StartCoroutine(Rush());
+                    
                     timer = 0;
                 }
             }
@@ -69,73 +62,92 @@ public class Boss0 : FollowEnemy
         }
     }
 
-    IEnumerator FallDown()
+    private void RandomSkill(int[] seed)
     {
-        while (transform.position.y <jumpHeight)
+        int total = seed.Sum();
+        int randint = Random.Range(0, total - 1);
+        for(int i = 1; i < seed.Length; i++)
         {
-
-            control.velocity.y = jumpSpeed;
-            //transform.position = new Vector3(transform.position.x, transform.position.y + 0.24f, transform.position.z);
-            yield return new WaitForEndOfFrame();
+            seed[i] += seed[i - 1];
         }
-        this.transform.position = new Vector2(Mathf.Clamp(player.transform.position.x,leftBound,rightBound), jumpHeight);
-        var attack = Instantiate(waringArea);
-        attack.transform.position = new Vector2(transform.position.x, nowHeight-1);
-        var attackArea = attack.transform.GetChild(0);
-        while (transform.position.y > nowHeight)
+        for (int i = 0; i < seed.Length; i++)
         {
-            //transform.position = new Vector2(transform.position.x, transform.position.y - 0.2f);
-            control.velocity.y = -jumpSpeed;
-            float dis=transform.position.y- 0.1f;
-            attackArea.transform.localScale = new Vector3(1 - dis / 20, 1 - dis / 20, 1);
-            yield return new WaitForEndOfFrame();
+            if(randint < seed[i])
+            {
+                
+                skillManager.emitters[i].EmitSkill();
+                break;
+            }
         }
-        Destroy(attack);
-        timer = 0;
+        
     }
 
-    IEnumerator Rush()
-    {
-        var item = Instantiate(rushAtea);
-        item.transform.position=new Vector2(transform.position.x,transform.position.y-1); 
-        Vector2 forword = player.transform.position - transform.position;
-        var warn = item.transform.GetChild(0);
-        rushEffect.SetActive(true);
-        if (forword.x < 0)
-        {
-            float time = 1f;
-            while(time > 0)
-            {
-                warn.transform.localScale = new Vector3(1 - time, 1 - time, 1);
-                time -= 0.02f;
-                yield return null;
-            }
-            rushEffect.SetActive(false);
-            while(transform.position.x> Mathf.Clamp(item.transform.position.x - rushDis,leftBound,rightBound))
-            {
-                control.move.x = -3;
-                yield return null;
-            }
-        }
-        else
-        {
-            float time = 1f;
-            item.transform.localScale = -item.transform.localScale;
-            while (time > 0)
-            {
-                warn.transform.localScale = new Vector3(1 - time, 1 - time, 1);
-                time -= 0.02f;
-                yield return null;
-            }
-            rushEffect.SetActive(false);
-            while (transform.position.x < Mathf.Clamp(item.transform.position.x + rushDis, leftBound,rightBound))
-            {
-                control.move.x = 3;
-                yield return null;
-            }
-        }
-        control.move.x = 0;
-        Destroy(item);
-        timer = 0;
-    }
+    //IEnumerator FallDown()
+    //{
+    //    while (transform.position.y <jumpHeight)
+    //    {
+    //        control.velocity.y = jumpSpeed;
+    //        //transform.position = new Vector3(transform.position.x, transform.position.y + 0.24f, transform.position.z);
+    //        yield return new WaitForEndOfFrame();
+    //    }
+    //    this.transform.position = new Vector2(Mathf.Clamp(player.transform.position.x,leftBound,rightBound), jumpHeight);
+    //    var attack = Instantiate(waringArea);
+    //    attack.transform.position = new Vector2(transform.position.x, nowHeight-1);
+    //    var attackArea = attack.transform.GetChild(0);
+    //    while (transform.position.y > nowHeight)
+    //    {
+    //        //transform.position = new Vector2(transform.position.x, transform.position.y - 0.2f);
+    //        control.velocity.y = -jumpSpeed;
+    //        float dis=transform.position.y- 0.1f;
+    //        attackArea.transform.localScale = new Vector3(1 - dis / 20, 1 - dis / 20, 1);
+    //        yield return new WaitForEndOfFrame();
+    //    }
+    //    Destroy(attack);
+    //    timer = 0;
+    //}
+
+    //IEnumerator Rush()
+    //{
+    //    var item = Instantiate(rushAtea);
+    //    item.transform.position=new Vector2(transform.position.x,transform.position.y-1); 
+    //    Vector2 forword = player.transform.position - transform.position;
+    //    var warn = item.transform.GetChild(0);
+    //    rushEffect.SetActive(true);
+    //    if (forword.x < 0)
+    //    {
+    //        float time = 1f;
+    //        while(time > 0)
+    //        {
+    //            warn.transform.localScale = new Vector3(1 - time, 1 - time, 1);
+    //            time -= 0.02f;
+    //            yield return null;
+    //        }
+    //        rushEffect.SetActive(false);
+    //        while(transform.position.x> Mathf.Clamp(item.transform.position.x - rushDis,leftBound,rightBound))
+    //        {
+    //            control.move.x = -3;
+    //            yield return null;
+    //        }
+    //    }
+    //    else
+    //    {
+    //        float time = 1f;
+    //        item.transform.localScale = -item.transform.localScale;
+    //        while (time > 0)
+    //        {
+    //            warn.transform.localScale = new Vector3(1 - time, 1 - time, 1);
+    //            time -= 0.02f;
+    //            yield return null;
+    //        }
+    //        rushEffect.SetActive(false);
+    //        while (transform.position.x < Mathf.Clamp(item.transform.position.x + rushDis, leftBound,rightBound))
+    //        {
+    //            control.move.x = 3;
+    //            yield return null;
+    //        }
+    //    }
+    //    control.move.x = 0;
+    //    Destroy(item);
+    //    timer = 0;
+    //}
 }
